8669633a4fea562fb0a0b6612ce3d071b493aced
Carson Waites <carsonrwaites@gmail.com>
2026-03-03 19:45:44 -0500

    Simplify class architecture to rely on one parent class

--- randomprocesses/randomProcesses.py
+++ randomprocesses/randomProcesses.py
@@ -2,26 +2,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
-class BernoulliProcess:
-    def __init__(self, p=0.5):
-        self.p=p
-
-    def simulate(self, n_periods, n_sims=1):
+class StochasticProcess:
+    def __init__(self, init_value=0.0, dt=None, title=None):
+        self.init_value = init_value
+        self.dt = dt
         self.paths = None
-        self.n_periods = n_periods
-        self.n_sims = n_sims
-        x_t = (np.random.uniform(size=(n_periods, n_sims))>(1-self.p))*1
-        self.paths = x_t
-        return x_t
+        self.time_index = None
 
-    def show_paths(self):
+    def simulate(self, n_periods, n_sims=1, seed=None):
+        if seed is not None:
+            np.random.seed(seed)
+        dt = self.dt
+        # Time index
+        if dt is None:  # Discrete process
+            self.time_index = np.arange(n_periods + 1)
+        else:           # Continuous process
+            n_steps = int(n_periods / dt)
+            self.time_index = np.linspace(0, n_periods, n_steps + 1)
+        self.paths = self._simulate_paths(self.time_index, n_sims)
+        return self.paths
+
+    def _simulate_paths(self, time_index, n_sims):
+         raise NotImplementedError
+
+    def show_paths(self, alpha=0.5):
+        if self.paths is None:
+            raise ValueError("Run simulate() first")
         fig, ax = plt.subplots()
-        ax.plot(self.paths,
-                c='blue',
-                alpha=0.5)
+        ax.plot(self.time_index, self.paths, alpha=alpha)
         ax.xaxis.set_major_locator(MaxNLocator(integer=True))
         plt.show()
 
+
+class BernoulliProcess(StochasticProcess):
+    def __init__(self, p=0.5, init_value=0.0, dt=None):
+        super().__init__(init_value, dt)
+        self.p=p
+
+    def _simulate_paths(self, time_index, n_sims):
+        n_periods = len(time_index)
+        x_t = (np.random.uniform(size=(n_periods, n_sims)) > (1 - self.p)) * 1
+        return x_t
+
+    # Yet to be implemented
     def get_distributions(self):
         # Get:
         # Binomial: The number of successes in the first n trials
@@ -29,157 +52,93 @@ class BernoulliProcess:
         # Geometric: The number of failures needed to get one success
         pass
 
-class RandomWalk:
-    def __init__(self, init_value=0):
-        self.init_value=init_value
-        self.paths = None
 
-    def simulate(self, n_periods, n_sims=1):
-        self.paths = None
-        self.n_periods = n_periods
-        self.n_sims = n_sims
-        noise = np.random.choice([1, -1], size=(n_periods, n_sims), replace=True)
+class RandomWalk(StochasticProcess):
+    def __init__(self, init_value=0.0, dt=None):
+        super().__init__(init_value, dt)
+        self.init_value=init_value
+
+    def _simulate_paths(self, time_index, n_sims):
+        n_periods = len(time_index)
+        noise = np.random.choice([1, -1], size=(n_periods, n_sims))
         noise[0, :] = 0
         noise = noise.cumsum(axis=0)
         x_t = np.zeros(shape=(n_periods, n_sims)) + self.init_value
         x_t += noise
-        self.paths = x_t
         return x_t
 
-    def show_paths(self):
-        fig, ax = plt.subplots()
-        ax.plot(self.paths,
-                c='blue',
-                alpha=0.5)
-        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-        plt.show()
 
-class BrownianMotion:
-    def __init__(self, mu=0.0, sigma=1.0, dt=100, init_value=0.0):
-        self.mu=mu
-        self.sigma=sigma
-        self.dt=dt
-        self.init_value = init_value
-        self.paths = None
-
-    def simulate(self, n_periods, n_sims=1):
-        self.paths = None
-        self.n_periods = n_periods
-        self.n_sims = n_sims
-        n_steps = n_periods * self.dt
-        noise = self.sigma * np.sqrt(1/self.dt) * np.random.normal(loc=0, scale=1, size=(n_steps, n_sims))
-        noise += self.mu * (1/self.dt)
-        noise[0, :] = 0
-        noise = noise.cumsum(axis=0)
-        x_t = np.zeros(shape=(n_steps, n_sims)) + self.init_value
-        x_t += noise
-        self.paths = x_t
-        return x_t
-
-    def show_paths(self):
-        fig, ax = plt.subplots()
-        ax.plot(np.linspace(0, self.n_periods, self.n_periods * self.dt),
-                self.paths,
-                c='blue',
-                alpha=0.5)
-        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-        plt.show()
-
-class OUProcess:
-    def __init__(self, mu=0.0, theta=1.0, sigma=1.0, dt=100, init_value=0.0):
-        self.mu=mu
-        self.theta=theta
-        self.sigma=sigma
-        self.dt=dt
-        self.init_value = init_value
-        self.paths = None
-
-    def simulate(self, n_periods, n_sims=1):
-        self.paths = None
-        self.n_periods = n_periods
-        self.n_sims = n_sims
-        n_steps = n_periods * self.dt
-        noise = self.sigma * np.sqrt(1/self.dt) * np.random.normal(loc=0, scale=1, size=(n_steps, n_sims))
-        x_t = np.zeros(shape=(n_steps, n_sims))
-        x_t[0, :] = self.init_value
-        for step in range(n_steps-1):
-            x_t[step+1, :] = x_t[step, :]*(1-(1/self.dt)*self.theta) + (1/self.dt)*self.theta*self.mu + noise[step, :]
-        self.paths = x_t
-        return x_t
-
-    def show_paths(self):
-        fig, ax = plt.subplots()
-        ax.plot(np.linspace(0, self.n_periods, self.n_periods * self.dt),
-                self.paths,
-                c='blue',
-                alpha=0.5)
-        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-        plt.show()
-
-class GeometricBM:
-    def __init__(self, mu=0.0, sigma=1.0, dt=100, init_value=1.0):
+class BrownianMotion(StochasticProcess):
+    def __init__(self, mu=0.0, sigma=1.0, dt=0.01, init_value=0.0):
+        super().__init__(init_value, dt)
         self.mu = mu
         self.sigma = sigma
+
+    def _simulate_paths(self, time_index, n_sims):
+        n_steps = len(time_index) - 1
+        drift = self.mu * self.dt
+        diffusion = self.sigma * np.sqrt(self.dt) * np.random.normal(size=(n_steps, n_sims))
+        increments = drift + diffusion
+        increments = np.vstack([np.zeros(n_sims), increments])
+        return self.init_value + np.cumsum(increments, axis=0)
+
+
+class OUProcess(StochasticProcess):
+    def __init__(self, mu=0.0, theta=1.0, sigma=1.0, dt=0.01, init_value=0.0):
+        super().__init__(init_value)
+        self.mu = mu
+        self.theta = theta
+        self.sigma = sigma
         self.dt = dt
-        self.init_value = init_value
-        self.paths = None
 
-    def simulate(self, n_periods, n_sims=1):
-        self.paths = None
-        self.n_periods = n_periods
-        self.n_sims = n_sims
-        n_steps = n_periods * self.dt
-        noise = self.sigma * np.sqrt(1 / self.dt) * np.random.normal(loc=0, scale=1, size=(n_steps, n_sims))
-        noise += (self.mu - 0.5*np.square(self.sigma)) * (1 / self.dt)
-        noise[0, :] = 0
-        noise = noise.cumsum(axis=0)
-        noise = np.exp(noise)
-        x_t = np.zeros(shape=(n_steps, n_sims)) + self.init_value
-        x_t *= noise
-        self.paths = x_t
-        return x_t
+    def _simulate_paths(self, time_index, n_sims):
+        n_steps = len(time_index) - 1
+        X = np.zeros((n_steps + 1, n_sims))
+        X[0, :] = self.init_value
 
-    def show_paths(self):
-        fig, ax = plt.subplots()
-        ax.plot(np.linspace(0, self.n_periods, self.n_periods * self.dt),
-                self.paths,
-                c='blue',
-                alpha=0.5)
-        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-        plt.show()
+        for step in range(n_steps):
+            dW = np.random.normal(size=n_sims) * np.sqrt(self.dt)
+            X[step+1, :] = X[step, :] + self.theta * (self.mu - X[step, :]) * self.dt + self.sigma * dW
+        return X
 
-class BrownianBridge:
-    def __init__(self, a=0.0, b=0.0, dt=100):
+
+class GeometricBM(StochasticProcess):
+    def __init__(self, mu=0.0, sigma=1.0, dt=0.01, init_value=1.0):
+        super().__init__(init_value, dt)
+        self.mu = mu
+        self.sigma = sigma
+
+    def _simulate_paths(self, time_index, n_sims):
+        n_steps = len(time_index) - 1
+        drift = (self.mu - 0.5 * self.sigma**2) * self.dt
+        diffusion = self.sigma * np.sqrt(self.dt) * np.random.normal(size=(n_steps, n_sims))
+        log_increments = drift + diffusion
+        increments = np.exp(np.vstack([np.zeros(n_sims), log_increments]))
+        S = self.init_value * np.cumprod(increments, axis=0)
+        return S
+
+
+class BrownianBridge(StochasticProcess):
+    def __init__(self, a=0.0, b=0.0, dt=0.01):
+        super().__init__(a, dt)
         self.a = a
         self.b = b
-        self.dt = dt
-        self.paths = None
 
-    def simulate(self, n_periods, n_sims=1):
-        self.paths = None
-        self.n_periods = n_periods
-        self.n_sims = n_sims
-        n_steps = n_periods * self.dt
-        noise = np.sqrt(1 / self.dt) * np.random.normal(loc=0, scale=1, size=(n_steps, n_sims))
+    def _simulate_paths(self, time_index, n_sims):
+        n_steps = len(time_index)
+        n_periods = n_steps * self.dt
+        noise = np.sqrt(1 * self.dt) * np.random.normal(loc=0, scale=1, size=(n_steps, n_sims))
         noise[0, :] = 0
         noise = noise.cumsum(axis=0)
         times = np.linspace(0, n_periods, n_steps).reshape(n_steps, 1) * np.ones(shape=(n_steps, n_sims))
-        #x_t = np.zeros(shape=(n_steps, n_sims)) + self.init_value
-        x_t = self.a*(1-times)/n_periods + self.b*(times/n_periods) + noise - (times/n_periods)*noise[-1:,]
-        self.paths = x_t
+        x_t = self.a * (1 - times) / n_periods + self.b * (times / n_periods) + noise - (times / n_periods) * noise[-1:,]
         return x_t
 
-    def show_paths(self):
-        fig, ax = plt.subplots()
-        ax.plot(np.linspace(0, self.n_periods, self.n_periods * self.dt),
-                self.paths,
-                c='blue',
-                alpha=0.5)
-        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-        plt.show()
 
+# ADD
 class PoissonProcess:
     pass
 
+# ADD
 class MarkovChain:
     pass
